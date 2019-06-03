d71ad1206b58f5df427357964e67548434065492
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-03 17:20:47 +0000

    fixed the buffer and added on.end ebent handler

--- printingBuffer.js
+++ printingBuffer.js
@@ -1,26 +1,31 @@
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
     var https = require('https'); 
 
     var requestOptions = {
       host: 'sytantris.github.io',
-      path: '/http-examples/step1.html'
+      path: '/http-examples/step2.html'
     };
     
     https.get(requestOptions, function (response) {
-        let buffer = '' 
+        let buffer = ''; // what other data type can I use???
 
         // set encoding of received data to UTF-8
         response.setEncoding('utf8');
-      
+        console.log('Response stream complete.');
         // the callback is invoked when a `data` chunk is received
         response.on('data', function (data) {
-            buffer += data;
-          console.log('All Chunks Received:', buffer);
+            buffer += data;          
         });
+        
+        response.on('end', function() {
+            console.log('All Data Received:', buffer);
+          });
+
+        //why not outputing???
 
     });
       
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
