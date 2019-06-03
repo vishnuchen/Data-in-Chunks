3f87ae2d6cb15da74b8e77e1822237eeec7b5963
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-03 16:45:51 +0000

    Printed data using response.on

---
+++ printingInChunks.js
@@ -0,0 +1,25 @@
+
+
+function getAndPrintHTMLChunks () {
+    var https = require('https'); 
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+    
+    https.get(requestOptions, function (response) {
+
+        // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+      
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function (data) {
+          console.log('Chunk Received:', data , '\n' );
+        });
+
+    });
+      
+}
+
+getAndPrintHTMLChunks();
